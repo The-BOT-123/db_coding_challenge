faab61b2cf4e8e7a6b299daca2377d6cc740379f
Sakshi Shah <sakshidshah24@gmail.com>
2023-08-04 17:41:51 +0530

    initial setup for spring backend with h2 database

---
+++ java-api/bonds_mgmt_backend/src/main/java/com/example/demo/BondsMgmtBackendApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BondsMgmtBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BondsMgmtBackendApplication.class, args);
+		
+		System.out.println("Application started!");
+	}
+
+}
