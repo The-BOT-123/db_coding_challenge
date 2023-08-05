4b9223da980da855793ab52a77080fa2ec1c0219
Samarth_2561 <samarthnaphade@gmail.com>
2023-08-05 23:32:21 +0530

    Updated the SQL Queries, and Reverted the application.properties back

--- java-api/bonds_mgmt_backend/src/main/java/com/db/model/User.java
+++ java-api/bonds_mgmt_backend/src/main/java/com/db/model/User.java
@@ -1,29 +1,74 @@
 package com.db.model;
 
-
-import jakarta.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
-@Table(name="usertable")
+@Table(name = "users")
 public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private Long id;
+	@Column(unique = true)
+	private String username;
+	@Column()
+	private String email;
+	private String password;
+	private String role;
 
-    public String getUsername() {
-        return username;
-    }
+	public User(String username, String email, String password, String role) {
+		this.username = username;
+		this.email = email;
+		this.password = password;
+		this.role = role;
+	}
 
-    @Column(name = "username", nullable = false)
-    private String username;
+	public Long getId() {
+		return id;
+	}
 
-    @Column(name = "password", nullable = false)
-    private String password;
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public String getPassword() {
-        return password;
-    }
-    // Other user fields, getters, setters, and constructors
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public User() {
+		super();
+	}
 }
-
