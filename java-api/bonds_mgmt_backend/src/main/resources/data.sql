2199735e8aed90e0114715e8fe752ab169a94bef
Samarth_2561 <samarthnaphade@gmail.com>
2023-08-07 12:35:37 +0530

    Updated Table Name and Data Query

--- java-api/bonds_mgmt_backend/src/main/resources/data.sql
+++ java-api/bonds_mgmt_backend/src/main/resources/data.sql
@@ -1,22 +1,22 @@
-INSERT INTO Users (id, username, role, email, password) VALUES (1, 'admin', 'admin','admin@gmail.com','$2a$10$rYc3NTOoXkuItSrEI9navekqajlJ.IS7ZfScu5xYLP..uwvcICvPi');
-INSERT INTO Users (id, username, role, email, password) VALUES (2, 'user', 'user','user@gmail.com','$2a$10$6Oxx8CREBurd.JZ0KK8mW.hcmZsaCjyC7h/xD4v1z2s1Z5PqeXiMW');
+INSERT INTO users (id, username, role, email, password) VALUES (1, 'admin', 'admin','admin@gmail.com','$2a$10$rYc3NTOoXkuItSrEI9navekqajlJ.IS7ZfScu5xYLP..uwvcICvPi');
+INSERT INTO users (id, username, role, email, password) VALUES (2, 'user', 'user','user@gmail.com','$2a$10$6Oxx8CREBurd.JZ0KK8mW.hcmZsaCjyC7h/xD4v1z2s1Z5PqeXiMW');
 
-INSERT INTO Books (book_name) VALUES ('test 1');
-INSERT INTO Books (book_name) VALUES ('test 2');
-INSERT INTO Books (book_name) VALUES ('test 3');
+INSERT INTO books (book_name) VALUES ('test 1');
+INSERT INTO books (book_name) VALUES ('test 2');
+INSERT INTO books (book_name) VALUES ('test 3');
 
-INSERT INTO BookUsers (book_id, user_id) VALUES (1,1);
-INSERT INTO BookUsers (book_id, user_id) VALUES (2,2);
+INSERT INTO bookusers (book_id, user_id) VALUES (1,1);
+INSERT INTO bookusers (book_id, user_id) VALUES (2,2);
 
-INSERT INTO CounterParty (name) VALUES ('DB');
-INSERT INTO CounterParty (name) VALUES ('CITI');
-INSERT INTO CounterParty (name) VALUES ('HSBC');
+INSERT INTO counterparty (name) VALUES ('DB');
+INSERT INTO counterparty (name) VALUES ('CITI');
+INSERT INTO counterparty (name) VALUES ('HSBC');
 
-INSERT INTO Security (issuer, maturity_date, coupon, type, face_value, status) VALUES ('CITI', '2023-09-04', 100, 'test', 120, 'not-mature');
-INSERT INTO Security (issuer, maturity_date, coupon, type, face_value, status) VALUES ('APPL', '2023-10-15', 120, 'test', 220, 'not-mature');
+INSERT INTO security (issuer, maturity_date, coupon, type, face_value, status) VALUES ('CITI', '2023-09-04', 100, 'test', 120, 'not-mature');
+INSERT INTO security (issuer, maturity_date, coupon, type, face_value, status) VALUES ('APPL', '2023-10-15', 120, 'test', 220, 'not-mature');
 
-INSERT INTO Trade (book_id, Counter_party_Id, Security_Id, Quantity, Status, Price, trade_type, trade_date, settlement_date) 
+INSERT INTO trade (book_id, counter_party_id, security_id, quantity, status, price, trade_type, trade_date, settlement_date) 
 VALUES (1,2,1, 120, 'pending', 250, 'buy', '2023-08-05 12:12:12', null);
 
-INSERT INTO Trade (book_id, Counter_party_Id, Security_Id, Quantity, Status, Price, trade_type, trade_date, settlement_date) 
+INSERT INTO trade (book_id, counter_party_id, security_id, quantity, status, price, trade_type, trade_date, settlement_date) 
 VALUES (2,2,2, 220, 'success', 350, 'buy', '2023-08-01 12:12:12', '2023-08-04 15:12:12');
--- java-api/bonds_mgmt_backend/src/main/resources/schema.sql
+++ java-api/bonds_mgmt_backend/src/main/resources/schema.sql
@@ -1,5 +1,5 @@
-CREATE TABLE IF NOT EXISTS Users (
-    id int NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT,
     username varchar(255) NOT NULL,
     role varchar(255) NOT NULL,
     email varchar(255) NOT NULL,
@@ -7,57 +7,57 @@ CREATE TABLE IF NOT EXISTS Users (
     PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS Books (
-    id int NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS books (
+    id INT NOT NULL AUTO_INCREMENT,
     book_Name varchar(255) NOT NULL,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS Security (
+CREATE TABLE IF NOT EXISTS security (
     id INT NOT NULL AUTO_INCREMENT,
-    Issuer varchar(255) NOT NULL,
-    Maturity_Date DATE NOT NULL,
-    Coupon BIGINT NOT NULL,
-    Type varchar(255) NOT NULL,
-    Face_value BIGINT NOT NULL,
-    Status ENUM('not-mature','mature', 'post-mature') NOT NULL,
+    issuer varchar(255) NOT NULL,
+    maturity_date DATE NOT NULL,
+    coupon BIGINT NOT NULL,
+    type varchar(255) NOT NULL,
+    face_value BIGINT NOT NULL,
+    status ENUM('not-mature','mature', 'post-mature') NOT NULL,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS CounterParty (
+CREATE TABLE IF NOT EXISTS counterparty (
     id INT NOT NULL AUTO_INCREMENT,
     name varchar(255) NOT NULL,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS BookUsers (
-    Book_Id INT NOT NULL,
-    User_Id INT NOT NULL,
+CREATE TABLE IF NOT EXISTS bookusers (
+    book_id INT NOT NULL,
+    user_id BIGINT NOT NULL,
     FOREIGN key
-        (Book_Id) REFERENCES Books (id),
+        (book_id) REFERENCES books (id),
     FOREIGN key
-        (User_Id) REFERENCES Users (id)
+        (user_id) REFERENCES users (id)
 );
 
 
-CREATE TABLE IF NOT EXISTS Trade (
+CREATE TABLE IF NOT EXISTS trade (
    id INT NOT NULL AUTO_INCREMENT,
    book_Id INT NOT NULL,
-   Counter_party_Id INT NOT NULL,
-   Security_Id INT NOT NULL,
-   Quantity BIGINT NOT NULL,
-   Status ENUM('success', 'pending', 'failed') NOT NULL,  
-   Price BIGINT NOT NULL,
+   counter_party_id INT NOT NULL,
+   security_id INT NOT NULL,
+   quantity BIGINT NOT NULL,
+   status ENUM('success', 'pending', 'failed') NOT NULL,  
+   price BIGINT NOT NULL,
    trade_type ENUM('buy','sell') NOT NULL,
    trade_date DATETIME NOT NULL,
    settlement_date DATETIME NULL,
    PRIMARY KEY (id),
    FOREIGN key
-       (book_Id) REFERENCES Books (id),
+       (book_Id) REFERENCES books (id),
    FOREIGN key
-       (Counter_party_Id) REFERENCES CounterParty (id),
+       (counter_party_id) REFERENCES counterparty (id),
    FOREIGN key
-       (Security_Id) REFERENCES Security (id)
+       (security_id) REFERENCES security (id)
 );
 
 
